0906444c415cc010cb76b39e57bd2d6dae8316d8
oakes <zach@sekao.net>
2020-12-20 09:21:02 -0500

    Start 0.7.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.sekao/odoyle-rum "0.6.0"
+(defproject net.sekao/odoyle-rum "0.7.0-SNAPSHOT"
   :description "Make Rum components with O'Doyle Rules"
   :url "https://github.com/oakes/odoyle-rum"
   :license {:name "Public Domain"
