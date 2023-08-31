9e796e6d2f92f36de2dce811df46a4734d897521
oakes <zach@sekao.net>
2023-08-31 17:07:54 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.sekao/odoyle-rum "1.1.0"
+(defproject net.sekao/odoyle-rum "1.3.0"
   :description "Make Rum components with O'Doyle Rules"
   :url "https://github.com/oakes/odoyle-rum"
   :license {:name "Public Domain"
